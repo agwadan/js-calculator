cecf27bcaa1ca15e0da8a3a509204c8d8a877770
agwadan <agwa.dan@gmail.com>
2023-07-24 13:38:37 +0300

    Create functions to handle the basic arithmetic

---
+++ index.js
@@ -0,0 +1,19 @@
+
+/* Function to add */
+const add = (x, y) => {
+  return x + y;
+ }
+const subtract = (x, y) => {
+  return x - y;
+}
+const multiply = (x, y) => {
+  return x * y;
+}
+const divide = (x, y) => {
+  return x / y;
+}
+
+console.log(add(2, 3))
+console.log(subtract(4, 2));
+multiply(3, 2);
+console.log(divide(10, 5));
